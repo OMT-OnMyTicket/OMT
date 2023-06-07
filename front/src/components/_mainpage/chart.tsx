56f5661628fe0080ce533055ad5da3afb6bacb07
HooniKim <tpgns2289@naver.com>
2023-06-07 21:34:57 +0900

    Home 기본적인 구성 완료

--- front/src/components/_mainpage/chart.tsx
+++ front/src/components/_mainpage/chart.tsx
@@ -1,36 +1,36 @@
 import styled from '../../styles/mainP_S/chart.module.css';
 
 const chart = () => {
+  let movieChart = ['1위', '2위', '3위', '4위', '5위'];
+
   return (
     <div className={styled.contents}>
       <div className={styled.chartTitle}>
-        <h4>무비차트</h4>
-        <h4>상영예정작</h4>
+        <h3>무비차트</h3>
+        <h3>상영예정작</h3>
       </div>
-      <div className={styled.chartList}>
-        <div className={styled.movies}>
-          <div>
-            <p className={styled.img}>영화이미지</p>
-            <div>영화 노출 등급</div>
-          </div>
 
-          <div>
-            <strong>영화제목</strong>
-            <span>
-              <p className={styled.img}>95%</p>
-            </span>
-            <span>예매율 00%</span>
-          </div>
-        </div>
-        <div>2등</div>
-        <div>3등</div>
-        <div>4등</div>
-        <div>5등</div>
-        <div>6등</div>
-        <div>7등</div>
-        <div>8등</div>
-        <div>9등</div>
-        <div>10등</div>
+      <div className={styled.chartList}>
+        {movieChart.map((a, i) => {
+          return (
+            <div className={styled.movies} key={i}>
+              <div>
+                <img
+                  src='/범죄도시.png'
+                  alt='영화 포스터'
+                  className={styled.movies}
+                />
+              </div>
+              <div className={styled.moviesTxt}>
+                <strong className={styled.movieTitle}>{a}영화제목</strong>
+                <div className={styled.moviesTxt2}>
+                  <span>95%</span>
+                  <span>예매율 00%</span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
