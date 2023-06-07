44174b2ff6fbbc049cee993446139d503651b060
HooniKim <tpgns2289@naver.com>
2023-06-07 15:33:18 +0900

    배포자동화 테스트

--- front/src/components/_mainpage/preview.tsx
+++ front/src/components/_mainpage/preview.tsx
@@ -5,7 +5,7 @@ const preview = () => {
     <div className={styled.allContents}>
       <div className={styled.preview}>
         <video className={styled.video}>영화 예고편</video>
-        <strong className={styled.title}>영화 제목</strong>
+        <strong className={styled.title}>배포 자동화 테스트</strong>
         <span className={styled.txt}>간략한 영화 설명</span>
         <div className={styled.txt2}>상세보기 or 예매하기</div>
       </div>
