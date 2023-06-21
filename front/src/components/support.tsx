ac1116b4fa7bc658910f5178da9db61aaee6270d
HooniKim <tpgns2289@naver.com>
2023-06-21 13:14:23 +0900

    scroll to Top 적용 완료

--- front/src/components/support.tsx
+++ front/src/components/support.tsx
@@ -1,14 +1,31 @@
 'use client';
 import Modal from '../components/modal';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styled from '../styles/support.module.css';
 
-const support = () => {
-  const [ModalOpen, setModalOpen] = useState(false);
+const Support = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [showScrollButton, setShowScrollButton] = useState(false);
 
   const handleClick = () => {
     setModalOpen(true);
   };
+
+  // 스크롤 이벤트가 발생했을 때만 move to Top 이 보이게하기
+
+  const handleScroll = () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    setShowScrollButton(scrollTop > 0);
+  };
+
+  const moveToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <>
@@ -17,10 +34,16 @@ const support = () => {
         className={styled.container}
         onClick={handleClick}
       />
-
-      {ModalOpen && <Modal setModalOpen={setModalOpen} />}
+      {modalOpen && <Modal setModalOpen={setModalOpen} />}
+      {showScrollButton && (
+        <img
+          src='/goToTop.svg'
+          onClick={moveToTop}
+          className={styled.goToTop}
+        />
+      )}
     </>
   );
 };
 
-export default support;
+export default Support;
