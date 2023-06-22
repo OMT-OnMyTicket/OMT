3f38fd28712ecd47ee014d0b655a63f579b7c094
HooniKim <tpgns2289@naver.com>
2023-06-22 13:29:23 +0900

    모바일 버전 완료

--- front/src/components/_mainpage/chart.tsx
+++ front/src/components/_mainpage/chart.tsx
@@ -92,7 +92,23 @@ const Chart = () => {
     slidesToShow: 5,
     slidesToScroll: 5,
     autoplay: true,
-    autoplaySpeed: 8000
+    autoplaySpeed: 8000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 4
+        }
+      }
+    ]
   };
 
   return (
