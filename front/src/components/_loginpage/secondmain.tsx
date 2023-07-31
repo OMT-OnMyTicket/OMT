b8fa4961a03f70cd76355e38f157a6e319465449
HooniKim <tpgns2289@naver.com>
2023-07-31 20:04:13 +0900

    login 두번쨰 페이지 완성

--- front/src/components/_loginpage/secondmain.tsx
+++ front/src/components/_loginpage/secondmain.tsx
@@ -15,22 +15,27 @@ const Second = () => {
     <>
       <div className={styled.second_Container}>
         <div
-          className={styled.ticketImgs2}
+          className={styled.left_Container}
           data-aos='fade-up'
-          data-aos-duration='3000'
-          id='Img'
+          data-aos-duration='2000'
         >
-          <img className={styled.ticketImg2_2} src='/ticket2.png' />
-          <img className={styled.ticketImg2_1} src='/ticket1.png' />
+          <h1 className={styled.mainTxt2_1}>TICKET ROME</h1>
+          <h2 className={styled.mainTxt2_2}>
+            재미있는 영화로 <br />
+            만드는 나만의 티켓
+          </h2>
+          <div data-aos='fade-right' data-aos-duration='1000' id='Img'>
+            <img className={styled.ticketImg2} src='/back.png' />
+          </div>
         </div>
-        <div
-          className={styled.second_Txt}
-          data-aos='fade-up'
-          data-aos-duration='3000'
-        >
-          재미있는 영화로 <br />
-          나만의 티켓을 <br />
-          만들어보세요
+        <div className={styled.right_Container}>
+          <div data-aos='fade-left' data-aos-duration='1000' id='Img'>
+            <img className={styled.ticketImg2} src='/front.png' />
+          </div>
+          <h3 className={styled.mainTxt2_3}>
+            재미있게 시청한 영화를 기록해보세요. <br />
+            나만의 추억을 만들어 한 눈에 확인할 수 있어요.
+          </h3>
         </div>
       </div>
     </>
