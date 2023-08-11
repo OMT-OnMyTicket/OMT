bacfe428d2154319b301e51b1c5c3ba3768d8883
HooniKim <tpgns2289@naver.com>
2023-08-11 19:23:32 +0900

    로그인 페이지 디자인 완성

--- front/src/components/_loginpage/loginpage.tsx
+++ front/src/components/_loginpage/loginpage.tsx
@@ -16,20 +16,22 @@ const Loginpage = () => {
     <>
       <div className={styled.login_Layout}>
         <div className={styled.login_Back}>
-          <div
-            className={styled.login_Txt}
-            data-aos='fade-up'
-            data-aos-duration='2000'
-          >
-            <p className={styled.login_Txt1}>LOGIN</p>
-            <p className={styled.login_Txt2}>소셜 로그인으로 간편하게.</p>
-          </div>
-          <div
-            className={styled.OauthBox}
-            data-aos='fade-up'
-            data-aos-duration='3000'
-          >
-            <Oauth />
+          <div>
+            <div
+              className={styled.login_Txt}
+              data-aos='fade-up'
+              data-aos-duration='2000'
+            >
+              <p className={styled.login_Txt1}>LOGIN</p>
+              <p className={styled.login_Txt2}>소셜 로그인으로 간편하게.</p>
+            </div>
+            <div
+              className={styled.OauthBox}
+              data-aos='fade-up'
+              data-aos-duration='3000'
+            >
+              <Oauth />
+            </div>
           </div>
           <div className={styled.Character_Position}>
             <img className={styled.Character} src='/캐릭터.png' />
--- front/src/components/_loginpage/oauth.tsx
+++ front/src/components/_loginpage/oauth.tsx
@@ -4,8 +4,40 @@ const Oauth = () => {
   return (
     <>
       <div className={styled.oauth_Layout}>
-        <div className={styled.login_Title}>간편하게 로그인하기</div>
-        <div className={styled.oauth_Box1}>
+        <ul>
+          <li className={styled.oauth_li}>
+            <div className={styled.oauth_li_Txt}>
+              <div className={styled.oauth_Naming_Layout}>
+                <div className={styled.oauth_name1}>네이버</div>
+                <img src='/네이버.png' className={styled.oauth_Logo} />
+              </div>
+              <div className={styled.Login_Txt}>네이버로 로그인하기</div>
+              <img src='/arrow.svg' className={styled.oauth_arrow} />
+            </div>
+          </li>
+          <li className={styled.oauth_li}>
+            <div className={styled.oauth_li_Txt}>
+              <div className={styled.oauth_Naming_Layout}>
+                <div className={styled.oauth_name2}>카카오</div>
+                <img src='/카카오.png' className={styled.oauth_Logo} />
+              </div>
+              <div className={styled.Login_Txt}>카카오로 로그인하기</div>
+              <img src='/arrow.svg' className={styled.oauth_arrow} />
+            </div>
+          </li>
+          <li className={styled.oauth_li}>
+            <div className={styled.oauth_li_Txt}>
+              <div className={styled.oauth_Naming_Layout}>
+                <div className={styled.oauth_name3}>구글</div>
+                <img src='/구글.png' className={styled.oauth_Logo} />
+              </div>
+              <div className={styled.Login_Txt}>구글로 로그인하기</div>
+              <img src='/arrow.svg' className={styled.oauth_arrow} />
+            </div>
+          </li>
+        </ul>
+
+        {/* <div className={styled.oauth_Box1}>
           <img src='/네이버.png' className={styled.oauth_Logo} />
           <p className={styled.oauth_Txt}>네이버로 로그인하기</p>
         </div>
@@ -16,7 +48,7 @@ const Oauth = () => {
         <div className={styled.oauth_Box3}>
           <img src='/구글.png' className={styled.oauth_Logo} />
           <p className={styled.oauth_Txt}>구글로 로그인하기</p>
-        </div>
+        </div> */}
       </div>
     </>
   );
