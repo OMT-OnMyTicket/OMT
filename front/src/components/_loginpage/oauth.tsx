ee160c5b93fe2fe8db4278de366a8cbf8a8bb38b
HooniKim <tpgns2289@naver.com>
2023-08-14 12:29:11 +0900

    로그인페이지 반응형 완성

--- front/src/components/_loginpage/oauth.tsx
+++ front/src/components/_loginpage/oauth.tsx
@@ -4,51 +4,41 @@ const Oauth = () => {
   return (
     <>
       <div className={styled.oauth_Layout}>
-        <ul>
+        <ul className={styled.oauth_ul}>
           <li className={styled.oauth_li}>
             <div className={styled.oauth_li_Txt}>
               <div className={styled.oauth_Naming_Layout}>
-                <div className={styled.oauth_name1}>네이버</div>
+                <div className={styled.oauth_name1}>Naver</div>
                 <img src='/네이버.png' className={styled.oauth_Logo} />
               </div>
               <div className={styled.Login_Txt}>네이버로 로그인하기</div>
+              <div className={styled.Login_Txt_M}>Login</div>
               <img src='/arrow.svg' className={styled.oauth_arrow} />
             </div>
           </li>
           <li className={styled.oauth_li}>
             <div className={styled.oauth_li_Txt}>
               <div className={styled.oauth_Naming_Layout}>
-                <div className={styled.oauth_name2}>카카오</div>
+                <div className={styled.oauth_name2}>Kakao</div>
                 <img src='/카카오.png' className={styled.oauth_Logo} />
               </div>
               <div className={styled.Login_Txt}>카카오로 로그인하기</div>
+              <div className={styled.Login_Txt_M}>Login</div>
               <img src='/arrow.svg' className={styled.oauth_arrow} />
             </div>
           </li>
           <li className={styled.oauth_li}>
             <div className={styled.oauth_li_Txt}>
               <div className={styled.oauth_Naming_Layout}>
-                <div className={styled.oauth_name3}>구글</div>
+                <div className={styled.oauth_name3}>Google</div>
                 <img src='/구글.png' className={styled.oauth_Logo} />
               </div>
               <div className={styled.Login_Txt}>구글로 로그인하기</div>
+              <div className={styled.Login_Txt_M}>Login</div>
               <img src='/arrow.svg' className={styled.oauth_arrow} />
             </div>
           </li>
         </ul>
-
-        {/* <div className={styled.oauth_Box1}>
-          <img src='/네이버.png' className={styled.oauth_Logo} />
-          <p className={styled.oauth_Txt}>네이버로 로그인하기</p>
-        </div>
-        <div className={styled.oauth_Box2}>
-          <img src='/카카오.png' className={styled.oauth_Logo} />
-          <p className={styled.oauth_Txt}>카카오로 로그인하기</p>
-        </div>
-        <div className={styled.oauth_Box3}>
-          <img src='/구글.png' className={styled.oauth_Logo} />
-          <p className={styled.oauth_Txt}>구글로 로그인하기</p>
-        </div> */}
       </div>
     </>
   );
