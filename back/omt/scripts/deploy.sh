3ea601cdbce5261cd964ea33bf9ffc1a62a5d0a5
JeeYulKim <86284203+JeeYulKim@users.noreply.github.com>
2023-09-19 18:34:10 +0900

    Update deploy.sh

--- back/omt/scripts/deploy.sh
+++ back/omt/scripts/deploy.sh
@@ -22,5 +22,6 @@ else
   sleep 5
 fi
 
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-sudo nohup java -jar omt-0.0.1-SNAPSHOT.jar >> /home/ubuntu/action/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/action/deploy.log 2>/home/ubuntu/action/deploy_err.log &
