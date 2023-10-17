4821d800ce4bbd16e557315cbde1caf9e7645bd5
HooniKim <tpgns2289@naver.com>
2023-10-17 17:24:47 +0900

    test

--- front/src/app/testpage/[...slug]/page.tsx
+++ front/src/app/testpage/[...slug]/page.tsx
@@ -1,15 +1,15 @@
 type PageParams = {
   slug: string;
 };
-export function generateStaticParams() {
-  return [{ slug: ['a', '1'] }, { slug: ['b', '2'] }, { slug: ['c', '3'] }];
+
+export async function generateStaticParams() {
+  const posts = await fetch('https://.../testpage').then((res) => res.json());
+
+  return posts.map((post: any) => ({
+    slug: post.slug
+  }));
 }
 
-export default function Page({ params }: { params: { slug: string[] } }) {
-  const { slug } = params;
-  return (
-    <div>
-      <div>테스트 페이지입니다. </div>
-    </div>
-  );
+export default function Page({ params }: { params: { slug: string } }) {
+  return <div>My Post: {params.slug}</div>;
 }
