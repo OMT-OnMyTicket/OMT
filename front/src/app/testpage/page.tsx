d079b022dd85db30c532ad4393ec69e1ccf291c7
HooniKim <tpgns2289@naver.com>
2023-10-17 15:51:52 +0900

    testPage

--- front/src/app/testpage/page.tsx
+++ front/src/app/testpage/page.tsx
@@ -1,9 +1,13 @@
 import Search from '../../components/_mainpage/search';
+import Link from 'next/link';
 
 const Test = () => {
   return (
     <div>
       <Search />
+      <Link href='/testpage/dasfk' as={'/testpage/dasfk'}>
+        <div>Click me!</div>
+      </Link>
     </div>
   );
 };
