9910e42a14cd05454e818fe39825195de88fa275
HooniKim <tpgns2289@naver.com>
2023-11-07 11:57:33 +0900

    공식페이지로 수정

--- front/src/app/testpage/[id]/page.tsx
+++ front/src/app/testpage/[id]/page.tsx
@@ -1,4 +1,11 @@
+// export function generateStaticParams() {
+//   return [{ id: '1' }, { id: '2' }, { id: '3' }];
+// }
+
+// export default function Page({ params }: { params: { id: string } }) {
+//   const { id } = params;
+//   return <div>{id} 페이지입니다.</div>;
+// }
 export default function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
-  return <div>{id} 페이지입니다.</div>;
+  return <div>My Post: {params.id}</div>;
 }
