7da251ea079e941209a329ef3a751b53a0e6cb7c
HooniKim <tpgns2289@naver.com>
2023-11-07 14:27:02 +0900

    test

--- front/src/app/testpage/[id]/page.tsx
+++ front/src/app/testpage/[id]/page.tsx
@@ -1,16 +1,24 @@
-// generateStaticParams 함수의 반환 타입을 정확히 지정합니다.
+type MovieState = {
+  movieTitle: string[];
+  movieSeq: string[];
+  movieId: string[];
+};
+
 export function generateStaticParams(
-  ids: any[] | undefined
-): { params: any }[] {
-  if (!Array.isArray(ids) || ids.length === 0) {
+  movieData: MovieState
+): { params: { id: string } }[] {
+  const { movieSeq } = movieData;
+
+  if (!Array.isArray(movieSeq) || movieSeq.length === 0) {
     return [];
   }
 
-  return ids.map((id) => ({ params: { id: String(id) } }));
+  return movieSeq.map((seq) => ({ params: { id: seq } }));
 }
-
 // Page 컴포넌트에서 params의 타입을 정확히 지정합니다.
+
 export default function Page({ params }: { params: { id: string } }) {
   const { id } = params;
+
   return <div>{id} 페이지입니다.</div>;
 }
