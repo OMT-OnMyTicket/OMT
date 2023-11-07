b095475b712cbdfa0e86b63c91b95f1bb42552ec
HooniKim <tpgns2289@naver.com>
2023-11-07 14:35:15 +0900

    test

--- front/src/app/testpage/[id]/page.tsx
+++ front/src/app/testpage/[id]/page.tsx
@@ -1,21 +1,9 @@
-type MovieState = {
-  movieTitle: string[];
-  movieSeq: string[];
-  movieId: string[];
-};
+'use client';
 
-export function generateStaticParams(
-  movieData: MovieState
-): { params: { id: string } }[] {
-  const { movieSeq } = movieData;
-
-  if (!Array.isArray(movieSeq) || movieSeq.length === 0) {
-    return [];
-  }
-
-  return movieSeq.map((seq) => ({ params: { id: seq } }));
+export function generateStaticParams() {
+  const MovieNumber = localStorage.getItem('MovieNum');
+  return [{ id: MovieNumber }];
 }
-// Page 컴포넌트에서 params의 타입을 정확히 지정합니다.
 
 export default function Page({ params }: { params: { id: string } }) {
   const { id } = params;
