2fe831a08146c97d85190792e3e16b1bd4b64f6d
JeeYulKim <86284203+JeeYulKim@users.noreply.github.com>
2023-11-20 17:45:16 +0900

    Update UserRepository.java

--- back/omt/src/main/java/twoman/omt/api/repository/user/UserRepository.java
+++ back/omt/src/main/java/twoman/omt/api/repository/user/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import twoman.omt.api.entity.user.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByEmail(String email);
 }
