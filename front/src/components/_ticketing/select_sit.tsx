412d3773059bfa8788509be4a24d414161114084
HooniKim <tpgns2289@naver.com>
2023-11-27 17:04:55 +0900

    좌석선택 가능 유무 적용

--- front/src/components/_ticketing/select_sit.tsx
+++ front/src/components/_ticketing/select_sit.tsx
@@ -19,15 +19,43 @@ const Select_Sit = () => {
   const maxSelectedSeats = Number(localStorage.인원수);
 
   // localStorage에서 예매정보를 가져옵니다.
-  const possibleSeatInfoString = localStorage.getItem('예매정보');
-  const possibleSeatInfo = possibleSeatInfoString
-    ? JSON.parse(possibleSeatInfoString)
+  const reservationInfoString = localStorage.getItem('예매정보');
+
+  // reservationInfoString이 존재하면 JSON 형식으로 파싱하여 객체로 변환합니다.
+  // 존재하지 않으면 기본값으로 빈 객체를 사용합니다.
+  const reservationInfo = reservationInfoString
+    ? JSON.parse(reservationInfoString)
     : {};
-  const possibleSeats = possibleSeatInfo.outSeat || 0;
+
+  const reservedSeatsFromLocalStorage = reservationInfo.outSeat || 0;
+
+  const [reservedSeats, setReservedSeats] = useState<string[]>([]);
 
   useEffect(() => {
     setAvailableSeats(maxSelectedSeats - selectedCount);
-  }, [selectedSeatLabels, maxSelectedSeats]);
+
+    // 랜덤으로 reservedSeatsFromLocalStorage 수만큼 좌석을 선택하여 검은색으로 설정
+    const randomlyReservedSeats: string[] = [];
+    while (randomlyReservedSeats.length < reservedSeatsFromLocalStorage) {
+      const randomRow = Math.floor(Math.random() * numRows);
+      const randomCol = Math.floor(Math.random() * numColumns);
+      const seatLabel = String.fromCharCode(65 + randomRow) + (randomCol + 1);
+      // 이미 선택되지 않은 좌석만 선택하도록 확인
+      if (!randomlyReservedSeats.includes(seatLabel)) {
+        randomlyReservedSeats.push(seatLabel);
+      }
+    }
+    setReservedSeats(randomlyReservedSeats);
+    setSelectedSeats((prevSelectedSeats) => {
+      const newSelectedSeats = prevSelectedSeats.map((row) => [...row]);
+      for (const seat of randomlyReservedSeats) {
+        const row = seat.charCodeAt(0) - 65;
+        const col = parseInt(seat.slice(1)) - 1;
+        newSelectedSeats[row][col] = true;
+      }
+      return newSelectedSeats;
+    });
+  }, [reservedSeatsFromLocalStorage]);
 
   const toggleSeat = (row: number, col: number) => {
     if (availableSeats > 0 || selectedSeats[row][col]) {
@@ -81,29 +109,20 @@ const Select_Sit = () => {
     return seatGrid;
   };
 
-  const resetSeats = () => {
-    setSelectedSeats(
-      Array(numRows)
-        .fill(false)
-        .map(() => Array(numColumns).fill(false))
-    );
-    setSelectedSeatLabels([]);
-  };
-
   const allSelectedSeat = selectedSeatLabels.join(', ');
 
   // 다음단계 버튼
   const handleNextBtn = (id: string) => {
     if (selectedCount < localStorage.인원수) {
       alert(`${availableSeats}명의 좌석을 추가로 선택해주세요`);
-      resetSeats();
+
       return;
     } else if (selectedCount > localStorage.인원수) {
       alert(
         `선택한 좌석이 ${localStorage.인원수}명을 초과하였습니다. 다시 선택해주세요.`
       );
       // 선택한 좌석 초기화
-      resetSeats();
+
       return;
     }
     // 선택한 좌석이 인원수와 일치하면 다음 단계로 넘어감
@@ -130,13 +149,6 @@ const Select_Sit = () => {
         <div className={styled.Seat_Layout}>
           <div className={styled.Seat_Chair}>{renderSeatGrid()}</div>
         </div>
-        <div className={styled.Btn_Position}>
-          <button className={styled.resetBtn} onClick={resetSeats}>
-            다시 선택하기
-            {/* <img src={'/reset.svg'} /> */}
-          </button>
-        </div>
-
         <div
           className={styled.NextBtn}
           onClick={() => handleNextBtn(allSelectedSeat)}
