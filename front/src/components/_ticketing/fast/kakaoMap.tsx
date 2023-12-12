8ffa59221bea2e14269e3831d1e918f559ee7cf6
HooniKim <tpgns2289@naver.com>
2023-12-12 18:36:10 +0900

    지도에 3사 영화관 띄우기 적용

--- front/src/components/_ticketing/fast/kakaoMap.tsx
+++ front/src/components/_ticketing/fast/kakaoMap.tsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import styled from '../../../styles/ticketingP_S/map.module.css';
 import Script from 'next/script';
+declare const kakao: any;
 
 const KakaoMap: React.FC = () => {
   const CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_MAP_KEY;
-  const KAKAO_SDK_URL = `"//dapi.kakao.com/v2/maps/sdk.js?appkey=${CLIENT_ID}&libraries=services,clusterer"`;
+  const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${CLIENT_ID}&autoload=false&libraries=services`;
 
   const [currentLocation, setCurrentLocation] = useState({ lat: 0, lng: 0 });
   const [map, setMap] = useState<any>(null);
   const [marker, setMarker] = useState<any>(null);
-  const kakao = (window as any).kakao;
+  const [infowindow, setInfowindow] = useState<any>(null);
+  const [currentMarker, setCurrentMarker] = useState<any>(null);
 
   useEffect(() => {
-    // Get current location
+    // 현재 위치 가져오기
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -27,15 +29,14 @@ const KakaoMap: React.FC = () => {
       console.error('Geolocation is not supported by this browser.');
     }
   }, []);
+
   useEffect(() => {
-    // Load Kakao Map SDK
     const script = document.createElement('script');
     script.src = KAKAO_SDK_URL;
     script.async = true;
     document.head.appendChild(script);
 
     script.onload = () => {
-      // Initialize Kakao Map API
       kakao.maps.load(() => {
         const container = document.getElementById('map');
         const options = {
@@ -43,13 +44,52 @@ const KakaoMap: React.FC = () => {
             currentLocation.lat,
             currentLocation.lng
           ),
-          level: 3
+          level: 5
         };
 
         const mapInstance = new kakao.maps.Map(container, options);
         setMap(mapInstance);
+        const ps = new kakao.maps.services.Places();
 
-        // Add Marker
+        // Define a red marker image
+        const redMarkerImage = new kakao.maps.MarkerImage(
+          '/png/영화관마커.png',
+          new kakao.maps.Size(30, 30),
+          { offset: new kakao.maps.Point(15, 30) }
+        );
+
+        // Function to add markers with the red marker image
+        const addMarkers = (data: any, status: any) => {
+          if (status === kakao.maps.services.Status.OK) {
+            data.forEach((place: any) => {
+              const marker = new kakao.maps.Marker({
+                position: new kakao.maps.LatLng(place.y, place.x),
+                map: mapInstance,
+                title: place.place_name,
+                image: redMarkerImage
+              });
+
+              // Register a click event on the marker
+              kakao.maps.event.addListener(marker, 'click', function () {
+                // Update the currentMarker state
+                setCurrentMarker(place);
+
+                // Create and set content for the infowindow
+                const infowindowContent = `<div style="padding:5px;font-size:12px;">${place.place_name}</div>`;
+                infowindow.setContent(infowindowContent);
+
+                // Open the infowindow at the marker's position
+                infowindow.open(map, marker);
+              });
+            });
+          }
+        };
+
+        ps.keywordSearch('CGV', addMarkers);
+        ps.keywordSearch('메가박스', addMarkers);
+        ps.keywordSearch('롯데시네마', addMarkers);
+
+        // Add marker for current location
         const markerInstance = new kakao.maps.Marker({
           position: new kakao.maps.LatLng(
             currentLocation.lat,
@@ -63,14 +103,28 @@ const KakaoMap: React.FC = () => {
     };
   }, [currentLocation]);
 
+  useEffect(() => {
+    // Initialize the infowindow
+    if (map) {
+      const infowindowInstance = new kakao.maps.InfoWindow({
+        zIndex: 1
+      });
+
+      setInfowindow(infowindowInstance);
+
+      // Close the infowindow when the map is clicked
+      kakao.maps.event.addListener(map, 'click', function () {
+        infowindowInstance.close();
+      });
+    }
+  }, [map]);
+
   const handleCurrentLocation = () => {
     if (map && marker) {
-      // Move the map to the current location
       map.panTo(
         new kakao.maps.LatLng(currentLocation.lat, currentLocation.lng)
       );
 
-      // Move the marker to the current location
       marker.setPosition(
         new kakao.maps.LatLng(currentLocation.lat, currentLocation.lng)
       );
@@ -85,6 +139,7 @@ const KakaoMap: React.FC = () => {
           style={{ width: '100%', height: '500px' }}
           className={styled.kakaoMap}
         ></div>
+
         <div
           className={styled.current_location}
           onClick={handleCurrentLocation}
