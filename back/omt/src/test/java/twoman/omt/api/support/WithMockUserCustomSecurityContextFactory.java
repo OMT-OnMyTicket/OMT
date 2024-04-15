0f664d4972cade81ba2cf3539b01601f89a43e9c
JeeYulKim <86284203+JeeYulKim@users.noreply.github.com>
2024-04-15 23:31:48 +0900

    Update WithMockUserCustomSecurityContextFactory.java

--- back/omt/src/test/java/twoman/omt/api/support/WithMockUserCustomSecurityContextFactory.java
+++ back/omt/src/test/java/twoman/omt/api/support/WithMockUserCustomSecurityContextFactory.java
@@ -1,48 +1,48 @@
-package twoman.omt.api.support;
+// package twoman.omt.api.support;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.test.context.support.WithSecurityContextFactory;
-import twoman.omt.api.annotations.WithMockUserCustom;
-import twoman.omt.api.entity.user.User;
-import twoman.omt.oauth.entity.ProviderType;
-import twoman.omt.oauth.entity.RoleType;
-import twoman.omt.oauth.entity.UserPrincipal;
+// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+// import org.springframework.security.core.authority.SimpleGrantedAuthority;
+// import org.springframework.security.core.context.SecurityContext;
+// import org.springframework.security.core.context.SecurityContextHolder;
+// import org.springframework.security.test.context.support.WithSecurityContextFactory;
+// import twoman.omt.api.annotations.WithMockUserCustom;
+// import twoman.omt.api.entity.user.User;
+// import twoman.omt.oauth.entity.ProviderType;
+// import twoman.omt.oauth.entity.RoleType;
+// import twoman.omt.oauth.entity.UserPrincipal;
 
-import java.util.Collections;
+// import java.util.Collections;
 
-public class WithMockUserCustomSecurityContextFactory implements WithSecurityContextFactory<WithMockUserCustom> {
+// public class WithMockUserCustomSecurityContextFactory implements WithSecurityContextFactory<WithMockUserCustom> {
 
-    @Override
-    public SecurityContext createSecurityContext(WithMockUserCustom annotation) {
-        User user = new User(
-                annotation.id(),
-                "tester",
-                "tester@testet.com",
-                "NO_PASS",
-                "Y",
-                "qwydhywh",
-                ProviderType.GOOGLE,
-                RoleType.USER
-        );
-        user.setUserSeq(annotation.userSeq());
+//     @Override
+//     public SecurityContext createSecurityContext(WithMockUserCustom annotation) {
+//         User user = new User(
+//                 annotation.id(),
+//                 "tester",
+//                 "tester@testet.com",
+//                 "NO_PASS",
+//                 "Y",
+//                 "qwydhywh",
+//                 ProviderType.GOOGLE,
+//                 RoleType.USER
+//         );
+//         user.setUserSeq(annotation.userSeq());
 
-        UserPrincipal userPrincipal = new UserPrincipal(
-                user,
-                Collections.singletonList(new SimpleGrantedAuthority(RoleType.USER.getCode()))
-        );
+//         UserPrincipal userPrincipal = new UserPrincipal(
+//                 user,
+//                 Collections.singletonList(new SimpleGrantedAuthority(RoleType.USER.getCode()))
+//         );
 
-        UsernamePasswordAuthenticationToken authentication =
-                new UsernamePasswordAuthenticationToken(
-                        userPrincipal,
-                        "NO_PASS"
-                );
+//         UsernamePasswordAuthenticationToken authentication =
+//                 new UsernamePasswordAuthenticationToken(
+//                         userPrincipal,
+//                         "NO_PASS"
+//                 );
 
-        SecurityContext context = SecurityContextHolder.getContext();
-        context.setAuthentication(authentication);
+//         SecurityContext context = SecurityContextHolder.getContext();
+//         context.setAuthentication(authentication);
 
-        return context;
-    }
-}
+//         return context;
+//     }
+// }
