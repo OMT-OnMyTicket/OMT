215c316d63729b19c52e26a15f3142fe392e4e60
JeeYulKim <86284203+JeeYulKim@users.noreply.github.com>
2024-04-16 09:16:13 +0900

    Update OAuth2SuccessHandler.java

--- back/omt/src/main/java/twoman/omt/oauth/handler/OAuth2SuccessHandler.java
+++ back/omt/src/main/java/twoman/omt/oauth/handler/OAuth2SuccessHandler.java
@@ -28,7 +28,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
     public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
     public static final Duration ACCESS_TOKEN_DURATION = Duration.ofDays(1);
-    public static final String REDIRECT_PATH = "omt-onmyticket.vercel.app/login";
+    public static final String REDIRECT_PATH = "https://omt-onmyticket.vercel.app/login";
 
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
@@ -71,7 +71,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     }
     //엑세스 토큰을 패스에 추가
     private String getTargetUrl(String token){
-        return UriComponentsBuilder.fromUriString(REDIRECT_PATH)
+        return UriComponentsBuilder.fromPath(REDIRECT_PATH)
                 .queryParam("token", token)
                 .build()
                 .toUriString();
